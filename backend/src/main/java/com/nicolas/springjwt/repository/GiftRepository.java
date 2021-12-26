27b5f424079a3a630d456abaec8810d19d9b8da5
Nicolas Gaget <n.gaget69@gmail.com>
2021-12-26 23:37:20 +0100

    mise en place de la gestion du model gift

--- backend/src/main/java/com/nicolas/springjwt/repository/GiftRepository.java
+++ backend/src/main/java/com/nicolas/springjwt/repository/GiftRepository.java
@@ -1,2 +1,7 @@
-package com.nicolas.springjwt.repository;public class CadeauRepository {
+package com.nicolas.springjwt.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.nicolas.springjwt.models.Gift;
+
+public interface GiftRepository extends JpaRepository<Gift,Long> {
 }
